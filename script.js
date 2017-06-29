27bcdd41eeea280f29e3be27265ce4dcaf8827c1
Allen B <pingguo.ny+git@gmail.com>
2017-06-28 20:47:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+/* ??? */
 document.getElementById("start").onclick = function() {
   alert("Start! again");
 }
 
 var time_spent = 0;
 
+/* Function gets called for Instant Time */
 function instantTime(n) {
   time_spent += n;
+}
+
+
+document.getElementById("instant-time-btn").onclick = function() {
+  var n = Number(document.getElementById("instant-time")) | 0;
+  instantTime(n);
 }
