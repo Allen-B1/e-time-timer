029a7940cdfff4ddc2fcfb8e7c09643d757384b9
Allen B <pingguo.ny+git@gmail.com>
2017-08-10 11:02:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ document.getElementById("start").onclick = function() {
 
 var time_spent = 0;
 
+/* Instant Time - When you forgot to set a timer */
+
 /* Function gets called for Instant Time */
 function instantTime(n) {
   time_spent += n;
 }
-
 
 document.getElementById("instant-time-btn").onclick = function() {
   var n = Number(document.getElementById("instant-time").value) | 0;
