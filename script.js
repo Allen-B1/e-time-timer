3377bcc7a909e08d22651a3a9e634c1478a38c83
Allen B <pingguo.ny+git@gmail.com>
2017-10-11 20:01:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 /* ??? */
+
+/* Class: Timer
+ *
+ * Parameters:
+ *   max - 0
+ */
+function ETimer(max, name) {
+    this.name = name;
+    this.sec_left = 0;
+    this.max = 0;
+}
+ETimer.prototype.removeTime = function(secs) {
+    if(this.sec_left - secs > 0)
+        this.sec_left -= secs;
+    else
+        this.sec_left = 0;
+}
+// Returns a function that returns the time left
+ETimer.prototype.startTimer = function(secs) {
+    var timeStarted = Date.now();
+    return function() {
+        var val = secs - ((Date.now() - timeStarted) / 1000);
+        return val > 0 ? val : 0;
+    };
+}
+
 document.getElementById("start").onclick = function() {
-  alert("Start! again");
+   alert("Start! again");
 }
 
 var time_spent = 0;
@@ -9,10 +35,10 @@ var time_spent = 0;
 
 /* Function gets called for Instant Time */
 function instantTime(n) {
-  time_spent += n;
+    time_spent += n;
 }
 
 document.getElementById("instant-time-btn").onclick = function() {
-  var n = Number(document.getElementById("instant-time").value) | 0;
-  instantTime(n);
+    var n = Number(document.getElementById("instant-time").value) | 0;
+    instantTime(n);
 }
